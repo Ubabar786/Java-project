220ec194bddba9e6328ed6c7a1805111953c15f1
Ubabar786 <babaruzair@gmail.com>
2023-08-26 03:45:54 +0100

    "Java Project 1"

--- src/JavaProject1/T92ndLargest.java
+++ src/JavaProject1/T92ndLargest.java
@@ -4,12 +4,12 @@ public class T92ndLargest {
     public static void main(String[] args) {
         // Write a java program to find the second largest number in the array?
         int[][] numbers = {
-                {7, 66, 13, 49, 82},
+                {100, 66, 13, 49, 82},
                 {62, 31, 54, 84, 91},
-                {4, 59, 98, 71, 20}
+                {4, 59, 97, 71, 20}
         };
 
-        int largest = numbers[0][0];
+        int largest = numbers[0][1];
         int secondLargest = numbers[0][0];
 
         for (int[] a : numbers) {
